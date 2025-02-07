705a652dba73d06618589a235ee6d1cb5f4f4096
Nics44kk <nicoadajar85@gmail.com>
2025-02-07 15:37:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // src/App.js
-import React from "react";
-import "./styles.css";
-import VideoCall from "./VideoCall";
+
+import React from 'react';
+import './App.css';
+import VideoCall from './VideoCall';
 
 function App() {
   return (
     <div className="App">
-      <h1>Valentine's Video Call</h1>
       <VideoCall />
     </div>
   );

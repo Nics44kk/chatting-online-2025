e6c5037635468bf39c902305d66d45457593e6b2
Nics44kk <nicoadajar85@gmail.com>
2025-02-07 14:53:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -6,40 +7,52 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let users = [];
+const roomCapacity = 2; // Define how many users per room
+let rooms = {}; // Store rooms and users
 
+// Handle when a user connects
 io.on('connection', (socket) => {
     console.log('A user connected');
-    
-    // Add user to the waiting list
-    users.push(socket);
 
-    // If there is a user waiting for a match, pair them
-    if (users.length >= 2) {
-        const user1 = users.shift();
-        const user2 = users.shift();
+    let assignedRoom = null;
 
-        // Notify both users they are matched
-        user1.emit('receive-message', 'You are connected! Start chatting.');
-        user2.emit('receive-message', 'You are connected! Start chatting.');
-
-        // Message handling between the two users
-        user1.on('send-message', (message) => {
-            user2.emit('receive-message', message);
-        });
-
-        user2.on('send-message', (message) => {
-            user1.emit('receive-message', message);
-        });
-
-        // Disconnect handling
-        user1.on('disconnect', () => {
-            console.log('User 1 disconnected');
-        });
-        user2.on('disconnect', () => {
-            console.log('User 2 disconnected');
-        });
+    // Try to find an available room with space
+    for (let room in rooms) {
+        if (rooms[room].length < roomCapacity) {
+            assignedRoom = room;
+            break;
+        }
     }
+
+    // If no room is available, create a new room
+    if (!assignedRoom) {
+        assignedRoom = `room${Object.keys(rooms).length + 1}`;
+        rooms[assignedRoom] = [];
+    }
+
+    // Add the user to the assigned room
+    rooms[assignedRoom].push(socket.id);
+    socket.join(assignedRoom);
+    console.log(`User ${socket.id} joined ${assignedRoom}`);
+
+    // Inform users that they've been matched and are connected
+    socket.emit('receive-message', `You are connected to ${assignedRoom}. Start chatting!`);
+    socket.to(assignedRoom).emit('receive-message', `User ${socket.id} joined the chat in ${assignedRoom}.`);
+
+    // Handle sending messages within the room
+    socket.on('send-message', (message) => {
+        io.to(assignedRoom).emit('receive-message', message); // Broadcast to the same room
+    });
+
+    // Handle disconnection and clean up
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        // Remove user from the room
+        rooms[assignedRoom] = rooms[assignedRoom].filter(user => user !== socket.id);
+        if (rooms[assignedRoom].length === 0) {
+            delete rooms[assignedRoom];
+        }
+    });
 });
 
 server.listen(3000, () => {

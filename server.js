f1d2e344d5228c46892e053989d4a99b56fd76a5
Nics44kk <nicoadajar85@gmail.com>
2025-02-07 15:28:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,64 +1,45 @@
 // server.js
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const roomCapacity = 2; // Number of users per room
-let rooms = {}; // Store rooms and users
+// Keep track of connected users
+let users = [];
 
-// Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
-// Handle when a user connects
-io.on('connection', (socket) => {
-    console.log('A user connected');
+// When a new user connects
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-    let assignedRoom = null;
+  // Add user to the list of online users
+  users.push(socket.id);
 
-    // Find an available room with space
-    for (let room in rooms) {
-        if (rooms[room].length < roomCapacity) {
-            assignedRoom = room;
-            break;
-        }
-    }
+  // Pair two users when there are at least 2 users connected
+  if (users.length >= 2) {
+    const user1 = users.pop();
+    const user2 = users.pop();
 
-    // If no room is available, create a new room
-    if (!assignedRoom) {
-        assignedRoom = `room${Object.keys(rooms).length + 1}`;
-        rooms[assignedRoom] = [];
-    }
+    io.to(user1).emit("paired", { partner: user2 });
+    io.to(user2).emit("paired", { partner: user1 });
+  }
 
-    // Add user to the assigned room
-    rooms[assignedRoom].push(socket.id);
-    socket.join(assignedRoom);
-    console.log(`User ${socket.id} joined ${assignedRoom}`);
+  // Handle chat messages
+  socket.on("chat_message", (message) => {
+    io.to(socket.id).emit("chat_message", message);  // Echo the message back
+  });
 
-    // Inform the users that they are connected to a room
-    socket.emit('receive-message', `You are connected to ${assignedRoom}. Start chatting!`);
-    socket.to(assignedRoom).emit('receive-message', `User ${socket.id} joined the chat in ${assignedRoom}.`);
-
-    // Handle sending messages within the room
-    socket.on('send-message', (message) => {
-        io.to(assignedRoom).emit('receive-message', message); // Broadcast to the room
-    });
-
-    // Handle disconnection and clean up
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-        // Remove user from the room
-        rooms[assignedRoom] = rooms[assignedRoom].filter(user => user !== socket.id);
-        if (rooms[assignedRoom].length === 0) {
-            delete rooms[assignedRoom];
-        }
-    });
+  // When a user disconnects, remove them from the user list
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    users = users.filter((user) => user !== socket.id);
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server running on port 3000");
 });

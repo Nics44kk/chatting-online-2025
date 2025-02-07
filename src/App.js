361f235e8ee77a4b886d3e2928d889be5f839267
Nics44kk <nicoadajar85@gmail.com>
2025-02-07 15:30:38 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+import "./styles.css";
+import VideoCall from "./VideoCall";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Valentine's Video Call</h1>
+      <VideoCall />
+    </div>
+  );
+}
+
+export default App;
